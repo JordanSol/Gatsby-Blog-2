ffbeb5c3edbad7c5409a407c2f39b5a5fe56b6db
JordanSol <jordansolfreeman@outlook.com>
2021-07-25 12:04:33 -0700

    Hopefully fixed safari spacing bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,20 +8,20 @@ const Navbar = () => {
 
     return (
         <nav className={styles.container}>
-            <Link className={styles.logo} to="/">
-                <img src={logo} alt="Site Logo" width="30px" />
-            </Link>
-            <div className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <div className={isOpen ? styles.menu : `${styles.menu} ${styles.closed}`}>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/blog">Blog</Link>
-            </div>
+                <Link className={styles.logo} to="/">
+                    <img src={logo} alt="Site Logo" width="30px" />
+                </Link>
+                <div className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className={isOpen ? styles.menu : `${styles.menu} ${styles.closed}`}>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                    <Link to="/blog">Blog</Link>
+                </div>
         </nav>
     )
 }
