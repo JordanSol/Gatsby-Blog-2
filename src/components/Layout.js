ef117d964f2534c49d012bbc6b2f0a005978b31f
JordanSol <jordansolfreeman@outlook.com>
2021-08-03 01:41:11 -0700

    Fun particles! Yay!

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import ParticleBackground from './ParticleBackground'
 import '../styles/index.scss'
 
 import * as styles from './Layout.module.scss'
@@ -8,6 +9,9 @@ import * as styles from './Layout.module.scss'
 const Layout = (props) => {
     return (
         <div className={styles.container}>
+            <div className={styles.background}>
+                <ParticleBackground/>
+            </div>
             <div className={styles.content}>
                 <Navbar/>
                 {props.children}
---
+++ src/components/ParticleBackground.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Particles from 'react-particles-js'
+import particleConfig from './config/particle-config'
+
+const ParticleBackground = () => {
+    return (
+        <Particles params={particleConfig}>
+        
+        </Particles>
+    )
+}
+
+export default ParticleBackground
